a1197e32f369c2a220c48793b82241f1707068fb
Joshua Gomberoff <joshuagomberoff@gmail.com>
2020-06-03 20:27:35 +0000

    added stdin and game exit features

--- play.js
+++ play.js
@@ -1,7 +1,23 @@
-/**
- * Establishes connection with the game server
- */
 const { connect } = require('./client');
 
 console.log('Connecting ...');
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf-8');
+  stdin.resume();
+  stdin.data()
+  stdin.on('data', () => {
+    handleUserInput();
+  })
+  return stdin;
+};
+
+// If CTRL + C is pressed, exits game
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
