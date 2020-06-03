9c267278ec47be0453fc7afeb825768577008c10
Joshua Gomberoff <joshuagomberoff@gmail.com>
2020-06-03 21:47:33 +0000

    writing movement inputs to server

--- play.js
+++ play.js
@@ -1,23 +1,7 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 
 console.log('Connecting ...');
-connect();
+const conn = connect();
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf-8');
-  stdin.resume();
-  stdin.data()
-  stdin.on('data', () => {
-    handleUserInput();
-  })
-  return stdin;
-};
-
-// If CTRL + C is pressed, exits game
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
+setupInput(conn);
