998b8333d71c1bbb78f43e3066163df676c8b948
Joshua Gomberoff <joshuagomberoff@gmail.com>
2020-06-03 19:39:18 +0000

    exporting connect function

---
+++ client.js
@@ -0,0 +1,21 @@
+const net = require('net');
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: 'localhost',
+    port: 50541    
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  // receives message from server
+  conn.on('data', (message) => {
+    console.log('Data: ', message);
+  })
+
+  return conn;
+}
+
+module.exports = { connect };
