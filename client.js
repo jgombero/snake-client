f9a47373e43bb6740e99908b70c036bf7ec66d2b
Joshua Gomberoff <joshuagomberoff@gmail.com>
2020-06-03 22:25:17 +0000

    required constants

--- client.js
+++ client.js
@@ -1,20 +1,24 @@
 const net = require('net');
+const { IP, PORT } = require('./constants');
+const username = process.argv[2];
+
 /**
  * Establishes connection with the game server
  */
 const connect = function() {
   const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541    
+    host: IP,
+    port: PORT    
   });
+  
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
 
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
-    
+
     //prints name to the server
-    conn.write('Name: JIG');
+    conn.write('Name: ' + username);
   })
 
   // receives message from server
