c4505b1a8b8b05b954fb6769fc9857eb521f745e
Joshua Gomberoff <joshuagomberoff@gmail.com>
2020-06-03 19:38:46 +0000

    exported required connect function

--- play.js
+++ play.js
@@ -1,23 +1,7 @@
-const net = require('net');
-
 /**
  * Establishes connection with the game server
  */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541    
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-
-  // receives message from server
-  conn.on('data', (message) => {
-    console.log('Data: ', message);
-  })
-
-  return conn;
-}
+const { connect } = require('./client');
 
 console.log('Connecting ...');
 connect();
